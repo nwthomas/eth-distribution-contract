6658c8730a669b7dbb023868cc991e60acc3e78b
Nathan Thomas <nwthomas@me.com>
2021-10-12 23:45:53 -0700

    Complete contribute describe block for testing suite

--- test/EthDistributor.test.js
+++ test/EthDistributor.test.js
@@ -1,4 +1,9 @@
+const chai = require("chai");
+const { solidity } = require("ethereum-waffle");
 const { ethers } = require("hardhat");
+const { expect } = chai;
+
+chai.use(solidity);
 
 describe("EthDistributor", () => {
   describe("constructor", () => {
@@ -8,10 +13,10 @@ describe("EthDistributor", () => {
       await ethDistributor.deployed();
 
       const contributionLimit = await ethDistributor.contributionLimit();
-      expect(contributionLimit.toNumber()).toBe(10000000000);
+      expect(contributionLimit.toNumber()).to.equal(10000000000);
 
       const contributorsLimit = await ethDistributor.maximumContributors();
-      expect(contributorsLimit.toNumber()).toBe(100);
+      expect(contributorsLimit.toNumber()).to.equal(100);
     });
 
     test("instantiates with owner address correctly", async () => {
@@ -21,12 +26,58 @@ describe("EthDistributor", () => {
       await ethDistributor.deployed();
 
       const contractOwner = await ethDistributor.owner();
-      expect(contractOwner).toBe(owner.address);
+      expect(contractOwner).to.equal(owner.address);
     });
   });
 
   describe("updateContributionLimit", () => {
-    // finish
+    test("updates the contribution limit when the owner calls it", async () => {
+      const [owner] = await ethers.getSigners();
+      const EthDistributor = await ethers.getContractFactory("EthDistributor");
+      const ethDistributor = await EthDistributor.deploy(10000, 1);
+      await ethDistributor.deployed();
+
+      let contributionLimit = await ethDistributor.contributionLimit();
+      expect(contributionLimit.toNumber()).to.equal(10000);
+
+      const txn = await ethDistributor.updateContributionLimit(10, { from: owner.address });
+      await txn.wait();
+
+      contributionLimit = await ethDistributor.contributionLimit();
+      expect(contributionLimit.toNumber()).to.equal(10);
+    });
+
+    test("throws when a non-owner address tries to call it", async () => {
+      const [, secondary] = await ethers.getSigners();
+      const EthDistributor = await ethers.getContractFactory("EthDistributor");
+      const ethDistributor = await EthDistributor.deploy(10000, 1);
+      await ethDistributor.deployed();
+
+      let error;
+      try {
+        await ethDistributor.updateContributionLimit(10, { from: secondary.address });
+      } catch (newError) {
+        error = newError;
+      }
+
+      expect(error instanceof Error).to.equal(true);
+    });
+
+    test("throws when the new contribution limit is greater than the maximumContributionLimit", async () => {
+      const [owner] = await ethers.getSigners();
+      const EthDistributor = await ethers.getContractFactory("EthDistributor");
+      const ethDistributor = await EthDistributor.deploy(10000, 1);
+      await ethDistributor.deployed();
+
+      let error;
+      try {
+        await ethDistributor.updateContributionLimit(10 ** 18 + 1, { from: owner.address });
+      } catch (newError) {
+        error = newError;
+      }
+
+      expect(error instanceof Error).to.equal(true);
+    });
   });
 
   describe("withdrawAllAddressEther", () => {
