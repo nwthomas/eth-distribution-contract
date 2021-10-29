b03bcf3f7adc80a5ed7e2f900505dfe1367d3c59
Nathan Thomas <nwthomas@me.com>
2021-10-29 12:14:58 -0700

    Add coverage command

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,10 @@
 import dotenv from "dotenv";
 
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
-import "solidity-coverage";
 import "solidity-coverage";
 
 dotenv.config();
