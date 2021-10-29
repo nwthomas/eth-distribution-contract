f45795ef4707a8f183dc04dc5c9b246845f9e81a
Nathan Thomas <nwthomas@me.com>
2021-10-29 11:49:37 -0700

    Add solidity-coverage and update package.json

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,11 @@
-import * as dotenv from "dotenv";
+import dotenv from "dotenv";
 
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
+import "solidity-coverage";
 import "solidity-coverage";
 
 dotenv.config();
